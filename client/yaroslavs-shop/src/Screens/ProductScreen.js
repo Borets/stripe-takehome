7efebad52b38e65725d54432a2a6d47295b855af
Yaroslav Borets <yborets@gmail.com>
2020-09-26 20:55:48 -0700

    more features

--- client/yaroslavs-shop/src/Screens/ProductScreen.js
+++ client/yaroslavs-shop/src/Screens/ProductScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen (props) {
-    console.log(props.match.params.id);
-    const product = data.products.find(x => x._id === props.match.params.id);
+    const [qty, setQty] = useState(1);
+    const productDetails = useSelector(state => state.productDetails);
+    const { product, loading, error } = productDetails; 
+    const dispatch = useDispatch(); 
+
+    useEffect(() => {
+        dispatch(detailsProduct(props.match.params.id));
+        return () => {
+            //
+        };
+
+    }, [])
+
+    const HandeAddToCart = () => {
+        props.history.push("/cart/"+ props.match.params.id + "?qty=" + qty )
+
+     }
+
     return <div>
-        <div> 
+        <div className="back-to-result"> 
             <Link to="/">Back to Results</Link>
         </div>
+        {loading? <div>Loading...</div>:
+        error? <div>{error} </div>: 
+            (
         <div className="details">
             <div className="details-image">
                 <img src={product.image} alt="product"></img>
@@ -22,7 +42,7 @@ function ProductScreen (props) {
                         {product.rating} Stars {product.numReviews} Reviews
                     </li>
                     <li>
-                        <b>{product.price}</b>
+                    Price <b>${product.price}</b>
                     </li>
                     <li>
                         Description:
@@ -36,26 +56,31 @@ function ProductScreen (props) {
             <div className="details-action">
                 <ul>
                     <li>
-                    Price: {product.price}
+                    Price: ${product.price}
                     </li>
                     <li>
-                    Status: {product.status}
+                    Status:   {product.countInStock > 0? "In Stock": "Out of Stock"} 
                     </li>
                     <li>
-                    Qty: <select>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
+                    Qty: <select value={qty} onChange={(e) => {setQty(e.target.value)}}>
+                        {[...Array(product.countInStock).keys()].map(x=>
+                            <option key={x+1} value={x+1}>{x+1}</option>
+                        )}
                     </select>
                     </li>
                     <li>
-                        <button>Add to Cart</button>
+                        {product.countInStock > 0 && <button onClick={HandeAddToCart} className="button primary">Add to Cart</button>
+                        }
+                       
                     </li>
                     
                 </ul>
             </div>
         </div>
-        </div>
+        ) 
+     }
+
+</div>
+    
 } 
 export default ProductScreen; 
---
+++ server/data.js
@@ -0,0 +1,38 @@
+export default {
+    products:[
+    {
+        _id: '1',
+        name: 'Pin #1', 
+        image: '/images/d1.png',
+        category: 'pins',
+        price:60, 
+        brand: 'Nike',
+        rating:4.5,
+        numReviews: 10, 
+        countInStock: 6,
+
+    },
+    {
+        _id: '2',
+        name: 'Pin #2', 
+        image: '/images/d2.png',
+        category: 'pins',
+        price:20, 
+        brand: 'Nike',
+        rating:4.5,
+        numReviews: 10,
+        countInStock: 0,
+
+    },
+    {
+        _id: '3',
+        name: 'Pin #3', 
+        image: '/images/d3.png',
+        category: 'pins',
+        price:10, 
+        brand: 'Nike',
+        rating:4.5,
+        numReviews: 10,
+        countInStock: 2,
+    }]
+}
